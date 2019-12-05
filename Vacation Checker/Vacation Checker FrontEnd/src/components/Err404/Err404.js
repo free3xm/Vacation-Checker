fcb7e46e146485660100fd273189a71dab20d592
free3 <free3xm@gmail.com>
2019-12-05 23:10:51 +0200

    update

--- Vacation Checker/Vacation Checker FrontEnd/src/components/Err404/Err404.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import cls from "./Err404.module.css";
-
-export default () => (
-  <h2 className={cls.err}>404 not found</h2>
-);
--- Vacation Checker/Vacation Checker FrontEnd/src/components/User/VacationError/VacationError.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import cls from "./VacationError.module.css";
-
-export default props => (
-  <div className={cls.err}>
-    <img src="./info.svg"/>
-      <p>{props.err}</p>
-  </div>
-);
